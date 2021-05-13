9a2ef41cac9fd38e65f33b5cc960301ad098f2e5
Pranjal <71340514+roverboy773@users.noreply.github.com>
2021-05-13 17:10:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const port=process.env.PORT||5000;
 const path=require('path');
 const cors=require('cors');
 const corsOptions ={
-    origin:['http://localhost:3000','https://festive-goldberg-8b032b.netlify.app'], 
+    origin:['http://localhost:3000','https://cocky-cori-9d4fb7.netlify.app'], 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
