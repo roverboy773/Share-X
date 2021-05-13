38f89cc04b94b5c6e759aa711a071227051ae888
Pranjal Sharma <ps9395@gmail.com>
2021-05-13 14:18:42 +0530

    hosted url added to cors

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const port=process.env.PORT||5000;
 const path=require('path');
 const cors=require('cors');
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:['http://localhost:3000','https://share-x-24259.web.app'], 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
