4058c2cc20ddeb2397407641db185d52d25d77e4
Pranjal Sharma <ps9395@gmail.com>
2021-05-29 01:33:20 +0530

    made responsive

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const port=process.env.PORT||5000;
 const path=require('path');
 const cors=require('cors');
 const corsOptions ={
-    origin:['http://localhost:3000','https://share-x-24259.web.app'], 
+    origin:['http://localhost:3000','http://127.0.0.1:3000'], 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
