cda00690a854aadf13340fe6f18e71ccd080c92e
Pranjal <71340514+roverboy773@users.noreply.github.com>
2021-05-29 01:40:50 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const port=process.env.PORT||5000;
 const path=require('path');
 const cors=require('cors');
 const corsOptions ={
-    origin:['http://localhost:3000','https://cocky-cori-9d4fb7.netlify.app'], 
+    origin:['http://localhost:3000','https://cocky-cori-9d4fb7.netlify.app','http://127.0.0.1:3000'], 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
