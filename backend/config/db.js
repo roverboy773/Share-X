d587a95d16f05c27f71d8b210b4e551a999a74f4
Pranjal <71340514+roverboy773@users.noreply.github.com>
2021-12-11 15:05:51 +0530

    updated db

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mongoose=require("mongoose");
 
 function connectDB(){
-mongoose.connect(process.env.DB,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true,useFindAndModify:true})
+mongoose.connect("mongodb+srv://roverboy:sharmasharma1999@cluster0.kjtew.mongodb.net/file_sharing_app?retryWrites=true&w=majority",{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true,useFindAndModify:true})
 .then(()=>console.log("db connection successful"))
 .catch((err)=>console.log(err));
 }
