4c89cfaffa1094545ae1e48a8ee50db1661eb10c
Pranjal Sharma <ps9395@gmail.com>
2022-03-27 21:44:19 +0530

    latest

--- backend/middleware/checkForVirus.js
+++ backend/middleware/checkForVirus.js
@@ -3,6 +3,40 @@ const clamscanConfig=require('../config/clamscan')
 
 const checkForVirus=async(req,res,next)=>{
     req.clamscan = await new NodeClam().init(clamscanConfig)
+
+    async function scanFile(filePath) {
+        try {
+            const clamscan = await ClamScan;
+            const { is_infected, viruses } = await clamscan.scan_file(filePath);
+    
+            if (is_infected) {
+                console.log(`The file is INFECTED with ${viruses}`);
+                throw new Error('ERR_FILE_SCAN_INFECTED');
+            } else {
+                return 'CLEAN';
+            }
+        } catch (err) {
+            throw new Error(err);
+        }
+      }
+    
+    
+      function start() {
+        return scanFile('/dodgyfile.rtf');
+      }
+      
+      // Call start
+      (async() => {    
+    
+        try{
+        await start();
+        }
+        catch(err){
+            console.log("error: ",err);
+        }    
+        
+      })();
+    
     next()
 }
 module.exports=checkForVirus
